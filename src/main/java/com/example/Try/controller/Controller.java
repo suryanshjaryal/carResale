9e249a6bb1dfff9a1b9922863727f94147bee3af
AutomationTesting0612 <automationtesting0612@gmail.com>
2024-10-20 14:43:54 +0530

    fixed the issue

--- src/main/java/com/example/Try/controller/Controller.java
+++ src/main/java/com/example/Try/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.Try.controller;
 import com.example.Try.service.Service;
 import com.example.Try.model.CarResale;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @org.springframework.stereotype.Controller
@@ -18,5 +19,11 @@ public String create(@ModelAttribute("carResale") CarResale carResale)
 service.saveData(carResale);
 return "index";
 }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showForm(Model model) {
+        model.addAttribute("carResale", new CarResale()); // Initialize a new CarResale object
+        return "index"; // Returns the index.html template
+    }
 
 }
